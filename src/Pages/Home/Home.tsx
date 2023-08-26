2eb1c1f9822fb602520cdbeaefd1a6759586ee13
cseswapon <cseswaponsaha@gmail.com>
2023-08-27 01:36:15 +0600

    [MOD] - modify with a simple test in login and home components

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 const Home = () => {
+    
     return (
         <div>
             <h1>This is a Home Page</h1>
--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,4 +1,13 @@
 const Login = () => {
+    interface User {
+      name: string;
+      id: number;
+    }
+    const user: User = {
+      name: "Hayes",
+      id: 3,
+    };
+    console.log(user);
     return (
         <div>
             <h1>This is a Login Page</h1>
